d5c9a67748a8789a8265c208868f749996b00b2e
WRall <wm_rall@hotmail.com>
2016-08-25 20:22:36 -0700

    Add some commandline input options

--- irs1318_vision/src/org/usfirst/frc/team1318/vision/VisionConstants.java
+++ irs1318_vision/src/org/usfirst/frc/team1318/vision/VisionConstants.java
@@ -24,9 +24,6 @@ public class VisionConstants
             VisionConstants.CAMERA_RESOLUTION,
             VisionConstants.CAMERA_FPS);
 
-    // Settings for Lifecam USB camera
-    public static final int CAMERA_INDEX = 0;
-
     public static final Scalar HSV_FILTER_LOW = new Scalar(70, 150, 150); 
     public static final Scalar HSV_FILTER_HIGH = new Scalar(90, 255, 255);
 }
--- irs1318_vision/src/org/usfirst/frc/team1318/vision/VisionSystem.java
+++ irs1318_vision/src/org/usfirst/frc/team1318/vision/VisionSystem.java
@@ -79,7 +79,36 @@ public class VisionSystem implements Runnable
     {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-        MJPEGCameraReader cameraReader = new MJPEGCameraReader(VisionConstants.CAMERA_INDEX);
+        MJPEGCameraReader cameraReader;
+        if (args != null && args.length == 0 && args[0] != null && !args[0].equals(""))
+        {
+            String argument = args[0];
+
+            boolean isNumeric = false; 
+            int result = 0;
+            try
+            {
+                result = Integer.parseInt(argument);
+                isNumeric = true;
+            }
+            catch (NumberFormatException ex)
+            {
+            }
+
+            if (isNumeric)
+            {
+                cameraReader = new MJPEGCameraReader(result);
+            }
+            else
+            {
+                cameraReader = new MJPEGCameraReader(argument);
+            }
+        }
+        else
+        {
+            cameraReader = new MJPEGCameraReader(VisionConstants.CAMERA_MJPEG_URL);
+        }
+
         Thread cameraThread = new Thread(cameraReader);
         cameraThread.start();
 
