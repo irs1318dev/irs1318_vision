697dbb40826852d8d7f388f0f8bc9d147a6a97f1
WRall <wm_rall@hotmail.com>
2016-08-31 15:37:08 -0700

    Fix instructions and main commandline argument processing

--- irs1318_vision/src/org/usfirst/frc/team1318/vision/VisionSystem.java
+++ irs1318_vision/src/org/usfirst/frc/team1318/vision/VisionSystem.java
@@ -80,7 +80,7 @@ public class VisionSystem implements Runnable
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
         MJPEGCameraReader cameraReader;
-        if (args != null && args.length == 0 && args[0] != null && !args[0].equals(""))
+        if (args != null && args.length != 0 && args[0] != null && !args[0].equals(""))
         {
             String argument = args[0];
 
