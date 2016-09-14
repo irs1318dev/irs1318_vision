f692ffa13ea9ac3979cbc968e0d2e7a36101f3ac
WRall <wm_rall@hotmail.com>
2016-09-13 23:07:27 -0700

    Change IP address settings based on camera settings

--- irs1318_vision/src/org/usfirst/frc/team1318/vision/VisionConstants.java
+++ irs1318_vision/src/org/usfirst/frc/team1318/vision/VisionConstants.java
@@ -19,15 +19,21 @@ public class VisionConstants
 
     // Settings for AXIS IP-based Camera
     public static final int CAMERA_FPS = 30;
-    public static final String CAMERA_IP_ADDRESS = "169.254.7.31"; // "169.254.59.141";
+    public static final String CAMERA_IP_ADDRESS = "169.254.13.18"; // "169.254.7.31", "169.254.59.141";
+    public static final String CAMERA_USERNAME_PASSWORD = "root:1318";
     public static final int CAMERA_RESOLUTION_X = 320;
     public static final int CAMERA_RESOLUTION_Y = 240;
     public static final String CAMERA_RESOLUTION = String.format("%dx%d", VisionConstants.CAMERA_RESOLUTION_X, VisionConstants.CAMERA_RESOLUTION_Y);
-    public static final String CAMERA_USERNAME_PASSWORD = "root:1318";
-    public static final String CAMERA_MJPEG_URL =
+    public static final String CAMERA_MJPEG_PASS_URL =
         String.format(
             "http://%s@%s/mjpg/video.mjpg?resolution=%s&req_fps=%d&.mjpg",
             VisionConstants.CAMERA_USERNAME_PASSWORD,
+            VisionConstants.CAMERA_IP_ADDRESS,
+            VisionConstants.CAMERA_RESOLUTION,
+            VisionConstants.CAMERA_FPS);
+    public static final String CAMERA_MJPEG_URL =
+        String.format(
+            "http://%s/mjpg/video.mjpg?resolution=%s&req_fps=%d&.mjpg",
             VisionConstants.CAMERA_IP_ADDRESS,
             VisionConstants.CAMERA_RESOLUTION,
             VisionConstants.CAMERA_FPS);
